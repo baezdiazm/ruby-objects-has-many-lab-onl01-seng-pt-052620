7380bc0fc92907dcba2a349d971d15d9f8d0df02
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-17 20:44:41 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Post
+  attr_accessor :title, :author
+  @@all = []
+  @@post_count = 0
+
+  def initialize(title)
+    @title = title
+    @@all << self
+    @@post_count += 1
+    #self.author = Author.name
+  end
+
+  def author_name
+    if self.author == nil
+      nil
+    else
+      self.author.name
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end

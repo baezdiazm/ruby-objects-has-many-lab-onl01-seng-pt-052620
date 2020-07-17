2a8820e424a7bcbee0d06891ba9ae62b71e4c704
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-17 20:29:17 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  attr_accessor :name
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    Post.all << post
+    post.author = self
+    @@post_count +=1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+    @@post_count +=1
+  end
+
+  def posts
+    Post.all
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
